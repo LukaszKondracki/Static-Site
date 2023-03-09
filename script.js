9fc02ebe34cd926a1e9085c4ba5059df5e374669
Kondracki <technikum@kondracki.info>
2023-03-09 12:02:03 +0100

    More movement

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const elements = document.querySelectorAll('.move');
+const btn = document.getElementById('add');
 
 const r = Math.random;
+const mover = (e) => {
+
+    e.style.top = `${r() * window.innerHeight}px`;
+    e.style.left = `${r() * window.innerWidth}px`;
+    e.style.rotate = `${r() * 360}deg`;
+    e.style.backgroundColor = `hsl(${r() * 360}, 100%, 50%)`;
+
+};
+const interval = 500;
 
 console.log('timer start');
 elements.forEach(el => {
-    setInterval(() => {
+    setInterval(() => mover(el), interval);
+});
 
-        el.style.top = `${(r() * (window.innerHeight - 100)) + 50}px`;
-        el.style.left = `${(r() * (window.innerWidth - 100)) + 50}px`;
-        el.style.rotate = `${r() * 360}deg`;
-        el.style.backgroundColor = `hsl(${r() * 360}, 100%, 50%)`;
-    
-    }, 500);
+btn.addEventListener('click', () => {
+    const newEl = el.cloneNode(true);
+    setInterval(() => mover(newEl), interval);
 });
