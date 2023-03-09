2198a70b37729379c97a33874b151d10e52ee0cd
Kondracki <technikum@kondracki.info>
2023-03-09 11:44:42 +0100

    Fix?

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ elements.forEach(el => {
         el.style.top = `${r() * window.innerHeight}px`;
         el.style.left = `${r() * window.innerWidth}px`;
         el.style.rotate = `${r() * 360}deg`;
-        el.style.backgroundColor = `hsl(${r() * 360}deg 255 255)`;
+        el.style.backgroundColor = `hsl(${r() * 360}deg 100% 100%)`;
     
     }, 500);
 });
