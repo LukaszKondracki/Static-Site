cdd21bded00761e4d6670e43f72a940b7eef6dc9
Kondracki <technikum@kondracki.info>
2023-03-09 11:18:38 +0100

    Timer fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const el = document.getElementById('move');
 
+
+console.log('timer start')''
 setInterval(() => {
 
     el.style.top = Math.random() * window.innerHeight;
     el.style.left = Math.random() * window.innerWidth;
     el.style.rotate = Math.random() * 360;
 
-}, 10)
+}, 10);
