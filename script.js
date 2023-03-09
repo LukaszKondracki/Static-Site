2f48d20349b7c596de9b4ee75e0d4ea681a201f3
Kondracki <technikum@kondracki.info>
2023-03-09 11:51:53 +0100

    Even better flying

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ console.log('timer start');
 elements.forEach(el => {
     setInterval(() => {
 
-        el.style.top = `${(r() * (window.innerHeight - 50)) + 50}px`;
-        el.style.left = `${(r() * (window.innerWidth - 50)) + 50}px`;
+        el.style.top = `${(r() * (window.innerHeight - 100)) + 50}px`;
+        el.style.left = `${(r() * (window.innerWidth - 100)) + 50}px`;
         el.style.rotate = `${r() * 360}deg`;
         el.style.backgroundColor = `hsl(${r() * 360}, 100%, 50%)`;
     
