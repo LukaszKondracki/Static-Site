7e52fe312105b52f2ad5cf1e1198e88b7f7ca065
Kondracki <technikum@kondracki.info>
2023-03-09 11:46:40 +0100

    Optimization

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const elements = document.querySelectorAll('.move');
 
+const r = Math.random;
+
 console.log('timer start');
 elements.forEach(el => {
     setInterval(() => {
 
-        const r = Math.random;
-
         el.style.top = `${r() * window.innerHeight}px`;
         el.style.left = `${r() * window.innerWidth}px`;
         el.style.rotate = `${r() * 360}deg`;
-        el.style.backgroundColor = `hsl(${r() * 360}deg 100% 100%)`;
+        el.style.backgroundColor = `hsl(${r() * 360}, 100%, 100%)`;
     
     }, 500);
 });
